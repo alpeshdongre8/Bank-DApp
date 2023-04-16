3fc365b9b4facdd9d15c17b628f7c0062bf9ebc8
alpeshdongre8 <dongrealpesh9@gmail.com>
2023-04-16 21:49:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BankApp from './BankApp';
+
+function App() {
+  return (
+    <div className="App">
+      <BankApp/>
+    </div>
+  );
+}
+
+export default App;
